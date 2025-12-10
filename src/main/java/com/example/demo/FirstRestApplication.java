a188ff2791d927e9e2810c42aaa2d194e9e8bc1d
archit-8 <archit.19cs023@cambridge.edu.in>
2025-12-10 07:28:39 +0000

    commit commit

--- src/main/java/com/example/demo/FirstRestApplication.java
+++ src/main/java/com/example/demo/FirstRestApplication.java
@@ -11,6 +11,7 @@ public class FirstRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstRestApplication.class, args);
 		//build will start in every 5 in jinkins
+		//build will start in every 100 in jinkins
 	}
 
 }
